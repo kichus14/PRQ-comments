ac412a5399f12611dca29fa91a751111bbe8aefe
JE75608 <sachidanand.prasad@kbc.be>
2021-02-01 22:49:43 +0530

    Display all the comments

--- client/src/Constant.js
+++ client/src/Constant.js
@@ -1,9 +1,15 @@
 export const baseUrl = 'http://localhost:3000';
 
-export const category = ['Code Quality Improvement', 'Configuration', 'Suggestion','Clarification', 'Package.json Suggestion'];
-export const subCategory  = ['Code Quality Improvement','Unit Test cases','Code Quality Improvement','Configuration','Maia Component Change','Project setup files','Code Refactor Suggestion','Midtier Changes','Unit Test cases','Different Developer, Different Opinion','No Change', 'Not in Coding guidelines', 'Deviation from Design', 'Not in Design'];
-export const commonAction = ['Yes', 'No', 'NA'];
-export const actionItem = ['CTS', 'KBC','NA'];
-export const angularKBCPractice = ['KBC Practice', 'Common Angular', 'Common Angular/KBC Practice', 'NA'];
-export const mitigationApproach = ['Correct Documentation to be provided', 'Follow Best Coding Practice', 'NA'];
-export const commentStatus = ['Completed', 'Inprogress', 'Pending','Resolved', 'Decline','NA'];
+export const category = [{value:'Code Quality Improvement', label: 'Code Quality Improvement'}, {value:'Configuration', label:'Configuration'}, {value:'Suggestion', label:'Suggestion'},{value:'Clarification', label:'Clarification'}, {value:'Package.json Suggestion',label:'Package.json Suggestion'}];
+
+export const subCategory  = [{value:'Code Quality Improvement', label:'Code Quality Improvement'},{value:'Unit Test cases',label: 'Unit Test cases'},{value:'Code Quality Improvement',label:'Code Quality Improvement'},{value:'Configuration', label:'Configuration'},{value:'Maia Component Change',label:'Maia Component Change'},{value:'Project setup files',label:'Project setup files'},{value:'Code Refactor Suggestion',label:'Code Refactor Suggestion'},{value:'Midtier Changes',label:'Midtier Changes'},{value:'Unit Test cases',label:'Unit Test cases'},{value:'Different Developer, Different Opinion',label:'Different Developer, Different Opinion'},{value:'No Change',label:'No Change'},{value:'Not in Coding guidelines', label:'Not in Coding guidelines'}, {value:'Deviation from Design', label:'Deviation from Design'}, {value:'Not in Design', label:'Not in Design'}];
+
+export const  commonAction = [{value:'Yes',label:'Yes'}, {value:'No',label:'No'}, {value:'NA',label:'NA'}];
+
+export const actionItem = [{value:'CTS',label:'CTS'}, {value:'KBC',label:'KBC'},{value:'NA',label:'NA'}];
+
+export const angularKBCPractice = [{value:'KBC Practice',label:'KBC Practice'}, {value:'Common Angular',label:'Common Angular'}, {value:'Common Angular/KBC Practice',label:'Common Angular/KBC Practice'}, {value:'NA',label:'NA'}];
+
+export const mitigationApproach = [{value:'Correct Documentation to be provided',label:'Correct Documentation to be provided'}, {value:'Follow Best Coding Practice',label:'Follow Best Coding Practice'}, {value:'NA',label:'NA'}];
+
+export const commentStatus = [{value:'Completed',label:'Completed'}, {value:'Inprogress',label:'Inprogress'}, {value:'Pending',label:'Pending'},{value:'Resolved',label:'Resolved'}, {value:'Decline',label:'Decline'},{value:'NA',label:'NA'}];
--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,108 +1,194 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {baseUrl, category, subCategory, commonAction, actionItem, angularKBCPractice, mitigationApproach, commentStatus} from '../Constant';
-import '../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css';
+
+import BootstrapTable from 'react-bootstrap-table-next';
+import cellEditFactory, { Type } from 'react-bootstrap-table2-editor';
+import ToolkitProvider, { CSVExport } from 'react-bootstrap-table2-toolkit';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 function Comments(props) {
     const [commentList, setCommentListState] = useState([]);
     const [loading, setLoading] = useState(false);
     const commentData = [];
-    const cellEditProp = {
-      mode: 'dbclick',
-      blurToSave: true
-    };
+    let allComment = [];
 
-    const createCustomExportCSVButton = (onClick) => {
-      return (
-        <button style={ { color: 'red' } } onClick={ onClick }>Custom Export CSV Btn</button>
-      );
+    const {ExportCSVButton} = CSVExport;
+
+    const columns = [{
+        dataField: 'id',
+        text: 'Sl.No',
+        editable: (false),
+        style: {fontWeight: 'normal', width: '2%'}
+    }, {
+        dataField: 'screen',
+        text: 'Screen',
+        editable: (false)
+    }, {
+        dataField: 'comments',
+        text: 'Feedbacks',
+        editable: (false),
+        csvFormatter: (cell, row, rowIndex) => `${
+            cell.map((data, i) => {
+                    return (data.props.children[1]).toString() + '\n\n';
+                }
+            )}`
+    }, {
+        dataField: 'developer',
+        text: 'Developer',
+        editable: (false)
+    }, {
+        dataField: 'effortMin',
+        text: `Efforts Spent(in mins)`,
+        editable: (true)
+    }, {
+        dataField: 'category',
+        text: 'Category',
+        editor: {type: Type.SELECT, options: category}
+    }, {
+        dataField: 'subCategory',
+        text: `Sub Category`,
+        editor: {type: Type.SELECT, options: subCategory}
+    }, {
+        dataField: 'canbecoveredinLinting',
+        text: `Covered in Linting`,
+        editor: {type: Type.SELECT, options: commonAction}
+    }, {
+        dataField: 'partofTrainingornot',
+        text: `EPart of Training or not`,
+        editor: {type: Type.SELECT, options: commonAction}
+    }, {
+        dataField: 'commonAngular',
+        text: 'Common Angular/KBC Practice',
+        editor: {type: Type.SELECT, options: angularKBCPractice}
+    }, {
+        dataField: 'actionItems',
+        text: `Action Items`,
+        editor: {type: Type.SELECT, options: actionItem}
+    }, {
+        dataField: 'mitigationApproach',
+        text: 'Mitigation Approach',
+        editor: {type: Type.SELECT, options: mitigationApproach}
+    }, {
+        dataField: 'wasthisfeedbackprovidedearlier',
+        text: `Was this feedback provided earlier`,
+        editor: {type: Type.SELECT, options: commonAction}
+    }, {
+        dataField: 'source',
+        text: `Source`,
+        editable: (false)
+    }, {
+        dataField: 'PRQcommentsgivenby',
+        text: `Comments given by`,
+        editable: (false)
+    }, {
+        dataField: 'commentDate',
+        text: `Comment Date`,
+        editable: (false)
+    }, {
+        dataField: 'status',
+        text: `Status`,
+        editor: {type: Type.SELECT, options: commentStatus}
+    }];
+
+    const showAllComments = (commentData) => {
+        allComment.push(commentData.author.displayName+'('+new Date(commentData.createdDate).toLocaleDateString("en-IN")+')'+' - '+commentData.text);
+        if(commentData.comments.length > 0) {
+            commentData.comments.forEach(function (element){
+                if(Array.isArray(element.comments)) {
+                    showAllComments(element);
+                }
+            })
+        };
+        return allComment;
     }
 
-    const options = {
-      exportCSVBtn: createCustomExportCSVButton
-    };
-
     useEffect(() => {
-      setLoading(true);
-            let data = {
+        setLoading(true);
+        let data = {
             key: props.match.params.projectKey,
             name: props.match.params.projectName,
             id: props.match.params.id
         }
         axios.post(baseUrl+'/comments', data)
-        .then(res => {
-          let prqDetail = (res.data.detail).filter((data)=>data.id == props.match.params.id);
-          let count = 0;
-          res.data.data.values.filter((data)=>{
-                if(data.action === 'COMMENTED'){
-                    if(!(data.comment.text).includes("YAML validation success")){
-                      count++;
-                      commentData.push(
-                        {
-                          id: count,
-                          screen : prqDetail[0].screenName,
-                          feedback : (data.comment.text).replace(/`/g, ''),
-                          developer: prqDetail[0].name,
-                          effortMin : "",
-                          category: "",
-                          subCategory: "",
-                          canbecoveredinLinting: "",
-                          partofTrainingornot: "",
-                          commonAngular: "",
-                          actionItems: "",
-                          mitigationApproach: "",
-                          wasthisfeedbackprovidedearlier:"",
-                          source: (data.diff)? data.diff.destination.name : '',
-                          PRQcommentsgivenby: data.comment.author.displayName,
-                          commentDate: new Date(data.createdDate).toLocaleDateString("en-IN"),
-                          commentDateString: data.createdDate,
-                          status: ""
+            .then(res => {
+                let prqDetail = (res.data.detail).filter((data)=>data.id == props.match.params.id);
+                let count = 0;
+                res.data.data.values.filter((data)=>{
+
+                    if(data.action === 'COMMENTED'){
+                        if(!(data.comment.text).includes("YAML validation success") && !(data.comment.text).includes("WARN - Nexus RAW repository not found")){
+                            allComment = [];
+                            showAllComments(data.comment);
+                            count++;
+                            commentData.push(
+                                {
+                                    id: count,
+                                    screen : prqDetail[0].screenName,
+                                    comments: allComment.map((data, i)=><p id={i} key={i}> {data.toString()} </p>),
+                                    developer: prqDetail[0].name,
+                                    effortMin : "",
+                                    category: "",
+                                    subCategory: "",
+                                    canbecoveredinLinting: "",
+                                    partofTrainingornot: "",
+                                    commonAngular: "",
+                                    actionItems: "",
+                                    mitigationApproach: "",
+                                    wasthisfeedbackprovidedearlier:"",
+                                    source: (data.diff)? data.diff.destination.name : '',
+                                    PRQcommentsgivenby: data.comment.author.displayName,
+                                    commentDate: new Date(data.createdDate).toLocaleDateString("en-IN"),
+                                    commentDateString: data.createdDate,
+                                    status: ""
+                                }
+                            )
                         }
-                      )
                     }
-                }
-            });
-            setLoading(false);
-            setCommentListState(commentData);
-        })
-        .catch(err => console.log(err))
+                });
+                setLoading(false);
+                setCommentListState(commentData);
+            })
+            .catch(err => console.log(err))
     }, []);
-    
     return (
-      <div className="row nopadding">
-        <div className="col-md-12 nopadding">
-          <div className="h-100 row align-items-center mrg-200" style={{ display: loading  ? 'block': 'none'}}>
-              <div className="col-sm-12 offset-6">
-                  <div className="spinner-border my-auto" role="status">
-                      <span className="sr-only">Loading...</span>
-                  </div>
-              </div>
-          </div>
-          <div id="comment-table" className ="comment-data" style={{ display: loading  ? 'none': 'block'}}> 
-            <BootstrapTable data={commentList} striped hover cellEdit={ cellEditProp } options={ options } exportCSV pagination search={ true }>
-                <TableHeaderColumn isKey dataField='id' width='3%' columnTitle>Sl.No</TableHeaderColumn>
-                <TableHeaderColumn  dataField='screen' columnTitle tdStyle={ { whiteSpace: 'normal' } } >Screen</TableHeaderColumn>
-                <TableHeaderColumn dataField='feedback' width='15%' tdStyle={ { whiteSpace: 'normal' } } columnTitle editable={ false }>Feedbacks</TableHeaderColumn>
-                <TableHeaderColumn dataField='developer' columnTitle tdStyle={ { whiteSpace: 'normal' } } >Developer</TableHeaderColumn>
-                <TableHeaderColumn dataField='effortMin' columnTitle>Efforts Spent<br/> (in mins)</TableHeaderColumn>
-                <TableHeaderColumn  dataField='category' columnTitle tdStyle={ { whiteSpace: 'normal' } } editable={ { type: 'select', options: { values: category } } }>Category</TableHeaderColumn>
-                <TableHeaderColumn dataField='subCategory' columnTitle tdStyle={ { whiteSpace: 'normal' } } editable={ { type: 'select', options: { values: subCategory } } }>Sub <br/>Category</TableHeaderColumn>
-                <TableHeaderColumn  dataField='canbecoveredinLinting' columnTitle editable={ { type: 'select', options: { values: commonAction } } }>Can be <br/>covered in<br/> Linting</TableHeaderColumn>
-                <TableHeaderColumn dataField='partofTrainingornot' columnTitle editable={ { type: 'select', options: { values: commonAction } } }>Part of <br/>Training or <br/>not</TableHeaderColumn>
-                <TableHeaderColumn dataField='commonAngular' columnTitle  tdStyle={ { whiteSpace: 'normal' } }  editable={ { type: 'select', options: { values: angularKBCPractice } } }>Common <br/>Angular<br/>/KBC Practice</TableHeaderColumn>
-                <TableHeaderColumn  dataField='actionItems' columnTitle editable={ { type: 'select', options: { values: actionItem } } }>Action Items</TableHeaderColumn>
-                <TableHeaderColumn dataField='mitigationApproach' columnTitle tdStyle={ { whiteSpace: 'normal' } }  editable={ { type: 'select', options: { values: mitigationApproach } } }>Mitigation <br/>Approach</TableHeaderColumn>
-                <TableHeaderColumn dataField='wasthisfeedbackprovidedearlier' columnTitle editable={ { type: 'select', options: { values: commonAction } } }>Was this <br/>feedback <br/>provided <br/>earlier</TableHeaderColumn>
-                <TableHeaderColumn  dataField='source' columnTitle width='10%' tdStyle={ { whiteSpace: 'normal' } }>Source</TableHeaderColumn>
-                <TableHeaderColumn dataField='PRQcommentsgivenby' columnTitle tdStyle={ { whiteSpace: 'normal' } }>PRQ <br/>comments <br/>given by</TableHeaderColumn>
-                <TableHeaderColumn dataField='commentDate' columnTitle tdStyle={ { whiteSpace: 'normal' } }>Comment <br/>Date</TableHeaderColumn>
-                <TableHeaderColumn  dataField='status' columnTitle editable={ { type: 'select', options: { values: commentStatus } } }>Status</TableHeaderColumn>
-            </BootstrapTable>
-          </div>
+        <div className="row nopadding">
+            <div className="col-md-12 nopadding">
+                <div className="h-100 row align-items-center mrg-200" style={{ display: loading  ? 'block': 'none'}}>
+                    <div className="col-sm-12 offset-6">
+                            <div className="spinner-border my-auto" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                </div>
+                <div id="comment-table" className ="comment-data" style={{ display: loading  ? 'none': 'block'}}>
+                <ToolkitProvider
+                    keyField="id"
+                    data={ commentList }
+                    columns={ columns }
+                    exportCSV={ {
+                        fileName: 'comments.csv',
+                            blobType: 'text/plain;charset=utf-8'
+                    }}>
+                    {
+                        props => (
+                            <div>
+                            <ExportCSVButton { ...props.csvProps }>Export CSV!!</ExportCSVButton>
+                    <hr />
+                    <BootstrapTable
+                        { ...props.baseProps }
+                        cellEdit={ cellEditFactory({ mode: 'dbclick', blurToSave: true  }) }
+                        pagination={ paginationFactory() } />
+                    </div>
+                    )
+                    }
+                </ToolkitProvider>
+                </div>
+            </div>
         </div>
-      </div>  
     )
 }
-
 export default Comments;
